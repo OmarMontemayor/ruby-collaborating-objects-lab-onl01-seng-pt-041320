7640e83f211f0c1f8ef7b37fd79f8f4b824a7d70
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-11 00:25:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,14 @@
 class Song
   attr_accessor :name, :artist
+  
+  @@all = []
   def initialize(name, artist = nil)
     @name = name
     @artist = artist
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
