f13fd930998ec5f9a35fb2b1acb5d2af04b4227a
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-11 02:58:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,4 +16,11 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+  
+  def self.new_by_filename(file)
+    song_info = file.comp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
 end
