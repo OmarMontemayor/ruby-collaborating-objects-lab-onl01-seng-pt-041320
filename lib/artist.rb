72ea9ed4dbdbfbda1579cdb5ffc9f72e5f5d2a54
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-11 00:52:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,7 +19,7 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
   
-  def self.find_or_create_by_name(artist)
-    self.all.select{|artist| @name == artist} || @@all << self
+  def self.find_or_create_by_name(name)
+  
   end
 end
